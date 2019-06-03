835db7ea716812e8cc31b80bf752b9049f0c3714
backup-redz <43549296+backup-redz@users.noreply.github.com>
2019-06-03 06:51:01 +0200

    Add files via upload

--- C_RECEP.ino
+++ C_RECEP.ino
@@ -1,35 +1,38 @@
 void recep (char lettre) { // commun
-  if (lettre == ' ') {
-    return;
-  }
+
   delay(15);
   switch (lettre) {
-    
+
     case 'T':
-    {
-      if (getTemperature(&temperatureInterieur, 2, true) != READ_OK) {
-        debug(F("Erreur de lecture du capteur"));
-        return;
+      {
+        if (getTemperature(&temperatureInterieur, 2, true) != READ_OK) {
+          debug(F("Erreur de lecture du capteur"));
+          return;
+        }
+        if (getTemperature(&temperatureExterieur, 3, true) != READ_OK) {
+          debug(F("Erreur de lecture du capteur"));
+          return;
+        }
+        debug(String(temperatureInterieur, 5));
+        debug(String(temperatureExterieur, 5));
       }
-      if (getTemperature(&temperatureExterieur, 3, true) != READ_OK) {
-        debug(F("Erreur de lecture du capteur"));
-        return;
-      }
-      debug(String(temperatureInterieur, 5));
-      debug(String(temperatureExterieur, 5));
-    }
-    break;
-  
-  case 'B':
-    {
-      while (true) {
-        delay(10);
-        char test = Serial.read();
-        if (test == '$') {
-          break;
+      break;
+
+    case '*':
+      Blt.print("On est arrive");
+      break;
+
+    case 'B':
+      {
+        while (true) {
+          delay(10);
+          char test = Serial.read();
+          Blt.write(test);
+          if (test == '$') {
+            break;
+          }
         }
       }
-    }
-    break;
+      break;
   }
 }
